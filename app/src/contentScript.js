2bdebed370ecf478756fdb84369c917dd3e1fdd6
dhananjayansb <dhananjayansb@gmail.com>
2021-07-23 12:28:56 +0530

    Update contentScript.js

--- app/src/contentScript.js
+++ app/src/contentScript.js
@@ -22,27 +22,33 @@ function init() {
 
 function parseDOM() {
     let tag = targetElement.tagName.toLowerCase();
-    let idValue = targetElement.id;
-    let idPattern = `//*[@id='${idValue}']`;
-    let count = getCountofXPath(idPattern);
-    if (count == 1) {
-        idPattern = `//${tag}[@id='${idValue}']`;
-        console.log(idPattern);
-    }
     let attributes = targetElement.attributes;
     addAllXPathAttribute(attributes, tag)
 }
 
-function addAllXPathAttribute(attributes, tagName) {
-    console.log(attributes);
-    console.log(tagName);
+function addAllXPathAttribute(attributes, tagName, targetElement) {
+    // console.log(attributes);
+    // console.log(tagName);
     Array.prototype.slice.call(attributes).forEach(element => {
 
-        let temp = `//${tagName}[@${element.name}='${element.value}']`;
-        let count = getCountofXPath(temp);
-        if (count == 1) {
-            console.log(temp);
+        switch (element.name) {
+            case "id":
+                getUniqueId(targetElement, tagName);
+                break;
+            case "name":
+                getUniqueName(targetElement, tagName);
+                break;
+                getUniqueClassName(targetElement, tagName);
+                break;
+            default:
+                break;
         }
+
+        // let temp = `//${tagName}[@${element.name}='${element.value}']`;
+        // let count = getCountofXPath(temp);
+        // if (count == 1) {
+        //     console.log(temp);
+        // }
     });
 }
 
@@ -50,4 +56,45 @@ function getCountofXPath(xpath) {
     let count = document.evaluate(`count(${xpath})`, document, null, XPathResult.ANY_TYPE, null).numberValue;
     console.log(count);
     return count;
+}
+
+//id
+let xpathData = [];
+
+function getUniqueId(element, tag) {
+    let Value = element.id;
+    let idValue = `//*[@id='${Value}']`;
+    let count = getCountofXPath(idValue);
+    if (count == 1) {
+        xpathData.push(Value);
+    }
+}
+
+function getUniqueName(element, tag) {
+    let Value = element.name;
+    let nameValue = `//*[@name='${Value}']`;
+    let count = getCountofXPath(nameValue);
+    if (count == 1) {
+        xpathData.push(Value);
+    }
+}
+
+function getUniqueClassName(element, tag) {
+    let Value = element.name;
+    let classValue = `//*[@class='${Value}']`;
+    let count = getCountofXPath(classValue);
+    if (count == 1) {
+        xpathData.push(Value);
+    }
+}
+
+function getUniqueTagName(element, tag) {
+    let count = document.getElementsByTagName(tag).length;
+    if (count == 1) {
+        xpathData.push(tag);
+    }
+}
+
+function getUniqueLinkText(element, tag) {
+
 }
